124857d0282848bb65bb6ba110960145beb6c252
Abhishek Jachak <abhishekjachak22@gmail.com>
2024-06-19 00:32:04 +0530

    xaz

---
+++ comapanySpecific/infosys/RPG_game.cc
@@ -0,0 +1,3 @@
+/*While playing an RPG game, you were assigned to complete one of the hardest quests in this game. There are n monsters you’ll need to defeat in this quest. Each monster i is described with two integer numbers – poweri and bonusi. To defeat this monster, you’ll need at least poweri experience points. If you try fighting this monster without having enough experience points, you lose immediately. You will also gain bonusi experience points if you defeat this monster. You can defeat monsters in any order.
+
+The questt turned out to be very hard – you try to defeat the monsters but keep losing repeatedly. Your friend told you that this quest is impossible to complete. Knowing that, you’re interested, what is the maximum possible number of monsters you can defeat?*/
